b2a0b14bf0894b0d56439d31264b1b97b85b5081
Diego Huerta Ocaña <76068997+DiegoHuerta1@users.noreply.github.com>
2025-07-04 12:51:20 -0600

    Dist matrix in greedy

--- Heuristics/utils/initial_solutions.py
+++ Heuristics/utils/initial_solutions.py
@@ -7,13 +7,16 @@ from Heuristics.utils.utils import compute_distance_matrix
 from Heuristics.utils.utils_sg import get_feasible_elements, get_RLC, update_feasible
 
 
-def greedy_algorithm(graph: Graph, num_regions: int) -> dict:
+def greedy_algorithm(graph: Graph, num_regions: int,
+                     dist_matrix: NDArray) -> dict:
     """
     Greedy algorithm for generating initial solutions for LS
+
 
     Args:
         graph (Graph): Graph instance
         num_regions (int): Number of regions
+        dist_matrix (NDArray): Distance Matrix
 
     Returns:
         dict: Partition
@@ -23,13 +26,9 @@ def greedy_algorithm(graph: Graph, num_regions: int) -> dict:
     list_nodes = np.arange(graph.vcount())
     seeds = np.random.choice(list_nodes, size = num_regions, replace = False)
 
-    # get the matrix with production vectors
-    vectors = np.array(graph.vs["x"])
-
-    # define weigths on the edges
-    vectors_v = vectors[[v for v,u in graph.get_edgelist()]]
-    vectors_u = vectors[[u for v,u in graph.get_edgelist()]]
-    graph.es["w"] = np.sum( np.abs(vectors_v - vectors_u), axis=1)
+    # Only define weights if not yet defined
+    if "w" not in graph.es.attributes():
+        graph.es["w"] = [dist_matrix[v, u] for v, u in graph.get_edgelist()]
 
     # compute distances, from each seed
     dist_from_seeds = graph.distances(source = seeds,
